fefa04415b989015f3ff9fcaf24890c6c693c1d6
zhouchen <you@example.com1583741285@qq.com  git config --global user.name Your>
2019-05-25 20:18:52 +0800

    do database

--- README.md
+++ README.md
@@ -1,39 +1,26 @@
 # 大二数据库实验
 
 #### 介绍
-{**以下是码云平台说明，您可以替换此简介**
-码云是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN）。专为开发者提供稳定、高效、安全的云端软件开发协作平台
-无论是个人、团队、或是企业，都能够用码云实现代码托管、项目管理、协作开发。企业项目请看 [https://gitee.com/enterprises](https://gitee.com/enterprises)}
+> 数据库课程实验综合小实验（做一个项目实现增删改查功能）
+>
+> 大二下学期期末大作业！
 
-#### 软件架构
-软件架构说明
+#### 
 
+### 技术运用：
 
-#### 安装教程
-
-1. xxxx
-2. xxxx
-3. xxxx
-
-#### 使用说明
-
-1. xxxx
-2. xxxx
-3. xxxx
-
-#### 参与贡献
-
-1. Fork 本仓库
-2. 新建 Feat_xxx 分支
-3. 提交代码
-4. 新建 Pull Request
+​	<u>运用到的技术</u>：**bootstrap**+ **art-template** + **node.js** + **mysql** 
 
 
-#### 码云特技
 
-1. 使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2. 码云官方博客 [blog.gitee.com](https://blog.gitee.com)
-3. 你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解码云上的优秀开源项目
-4. [GVP](https://gitee.com/gvp) 全称是码云最有价值开源项目，是码云综合评定出的优秀开源项目
-5. 码云官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6. 码云封面人物是一档用来展示码云会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+话不多说：
+
+ 	用图说话：
+
+![mark](http://static.zxinc520.com/blog/20190525/JWCuqSFLOqbB.png?imageslim)
+
+![mark](http://static.zxinc520.com/blog/20190525/KHUkC8mo5c0L.png?imageslim)
+
+![mark](http://static.zxinc520.com/blog/20190525/Q4p9PqfySpYr.png?imageslim)
+
+![mark](http://static.zxinc520.com/blog/20190525/RdNGNjvSnJNF.png?imageslim)
---
+++ router.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const router = express.Router()
+const db=require('./models/mysql')
+
+
+
+router.get('/',function (req,res,next) {
+    db.query('select * from user', [],function(result,fields){
+        res.render('index.html',{
+            result:result
+        })
+    });
+})
+
+router.post('/add',function (req,res,next) {
+    let body = req.body
+     console.log(body)
+    let  addSql = 'INSERT INTO user(id,username,tel) VALUES(?,?,?)';
+    let  addSqlParams =[body.id, body.username,body.tel];
+    db.query(addSql,addSqlParams,function(result,fields){
+        console.log('添加成功')
+        return res.status(200).json({
+            err_code: 0,
+            message: 'OK'
+        })
+    })
+})
+
+router.get('/delete',function (req,res,next) {
+    console.log(req.query)
+    let  deleteSql = 'DELETE FROM user  WHERE id = ?';
+    let  deleteSqlParams =req.query.id;
+    db.query(deleteSql, deleteSqlParams,(err, results) => {
+        if(err){
+            console.log(err);
+        }
+        db.query('select * from user', [],function(result,fields){
+            res.render('index.html',{
+                result:result
+            })
+        });
+    })
+})
+
+
+router.get('/fix',function (req,res,next) {
+    fixsql='SELECT * FROM user WHERE id = ?'
+    fixSqlParams=req.query.id
+    db.query(fixsql, fixSqlParams,(result) => {
+        res.render('fix.html',{
+            result:result[0]
+        })
+    })
+
+
+})
+
+router.post('/fix',function (req,res,next) {
+    console.log(req.body)
+    updatasql='UPDATE user SET username = ?,tel=? WHERE id = ?';
+    updataSqlParams=[req.body.username, req.body.tel,req.body.id];
+    db.query(updatasql,updataSqlParams, (err, results) => {
+        if(err){
+            console.log(err);
+        }
+        return res.status(200).json({
+            err_code: 0,
+            message: 'OK'
+        })
+    })
+
+})
+
+router.post('/search',function (req,res,next) {
+    console.log(req.body)
+    arr=[]
+    db.query('select * from user', [],function(result,fields){
+       console.log(result)
+        result.forEach(function (e) {
+            if (e.id.indexOf(req.body.value)>=0||e.username.indexOf(req.body.value)>=0||e.tel.indexOf(req.body.value)>=0){
+                arr.push(e)
+            }
+        })
+        return res.status(200).json({
+            err_code: 0,
+            message: arr
+        })
+    });
+
+
+})
+
+module.exports = router;
